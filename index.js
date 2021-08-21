95a712d95b2c4f3498c1ec1a0a58a0ac33925bd4
danyalshah3 <72283767+danyalshah3@users.noreply.github.com>
2021-08-21 11:52:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 
+
+
+
+  function superbowlWin(record) {
+     
+    return record.find( (element) => 
+    element.result === "W"
+        
+    )
+ 
+    
+    }
+  
+    
+//   function superbowlWin(record){
+//    record.find(record.result === "W")
+// }
+
+
+//   console.log(record);
